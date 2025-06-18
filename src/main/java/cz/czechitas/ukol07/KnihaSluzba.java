746f4f432baf321efbf27b626b98e4098ff5b3e1
zdeni sykorova <sykorova@hotmail.com>
2025-06-18 08:04:24 +0200

    class Kniha, rozpracovano class KnihaSluzba

--- src/main/java/cz/czechitas/ukol07/KnihaSluzba.java
+++ src/main/java/cz/czechitas/ukol07/KnihaSluzba.java
@@ -1,8 +1,11 @@
 package cz.czechitas.ukol07;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
@@ -13,15 +16,25 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class KnihaSluzba {
-    ObjectMapper objectMapper = new ObjectMapper();
+    private List<Kniha> kniha;
 
-    //není dobře
-   try(InputStream inputStream = KnihaSluzba.class.getResourceAsStream("knihy.json"));
-
-    {
-        if (inputStream == null) {
-            throw new RuntimeException("Neni sluzba pro knihy.");
+    public static void main(String[] args) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try (InputStream inputStream = KnihaSluzba.class.getResourceAsStream("knihy.json")) {
+            if (inputStream == null) {
+                throw new FileNotFoundException("Soubor chybi.");
+            }
+            objectMapper.readValue(inputStream, new TypeReference<List<Kniha>>() {
+            });
         }
+
+        //Path kniha = Paths.get("kniha.json");
+        //try(InputStream inputStream = KnihaSluzba.class.getResourceAsStream("knihy.json")) {if (inputStream == null) {
+        //      //      throw new IOException("Neni sluzba pro knihy.");
+        //        //}}
+        //ObjectMapper objectMapper = new ObjectMapper();
+        //objectMapper.readValue(inputStream, new TypeReference<List<Kniha>>() {});
+
 
     }
 }
