32f5ed0983a953fd8aa100b105a027ef7da0ed11
zdeni sykorova <sykorova@hotmail.com>
2025-06-19 09:11:47 +0200

    class Kniha, class KnihaSluzba

--- src/main/java/cz/czechitas/ukol07/KnihaSluzba.java
+++ src/main/java/cz/czechitas/ukol07/KnihaSluzba.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -27,15 +28,38 @@ public class KnihaSluzba {
             objectMapper.readValue(inputStream, new TypeReference<List<Kniha>>() {
             });
         }
-
+        // TAKTO NEJSPÍŠ NE
         //Path kniha = Paths.get("kniha.json");
         //try(InputStream inputStream = KnihaSluzba.class.getResourceAsStream("knihy.json")) {if (inputStream == null) {
-        //      //      throw new IOException("Neni sluzba pro knihy.");
-        //        //}}
+        //     throw new IOException("Neni sluzba pro knihy.");
+        //     }}
         //ObjectMapper objectMapper = new ObjectMapper();
         //objectMapper.readValue(inputStream, new TypeReference<List<Kniha>>() {});
-
-
     }
+
+    public List<String> nacistSeznamKnih() {
+        List<String> seznamKnih = new ArrayList<>();
+        return seznamKnih;
+    }
+
+    public List<String> nacistKnihyAutora() {
+        List<String> knihyAutora = new ArrayList<>();
+        for (Kniha kniha : kniha) {
+            if (kniha.getAutor() != null) {
+                knihyAutora.add(kniha.getAutor());
+            }
+        }
+        return knihyAutora;
+    }
+
+    public List<Integer> nacistKnihyRokVydani() {
+        List<Integer> rokVydani = new ArrayList<>();
+        for (Kniha kniha : kniha) {
+            rokVydani.add(kniha.getRokVydani());
+        }
+        return rokVydani;
+    }
+
+
 }
 
